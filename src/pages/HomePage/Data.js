2ebdcbba88ad2aa517af080f543ec49bafa8784e
Quackenstien <donaldhawkins422@gmail.com>
2020-09-05 15:25:35 -0700

    style: add info to data file to build home page

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,3 +1,14 @@
 export const homeObjOne = {
   lightBg: false,
+
+  imgStart: "",
+  lightTopLine: true,
+  lightTextDesc: true,
+  lightText: true,
+  buttonLabel: "Get Started",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+  headline: "Lead Generation Specialist for Online Businesses.",
+  topLine: "Marketing Agency",
+  primary: true,
 };
