ce5c70277f14730c5436c0d97832846c7bd9cc0d
Quackenstien <donaldhawkins422@gmail.com>
2020-09-05 14:01:49 -0700

    refactor: removed excess code

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -28,9 +28,15 @@ const Navbar = () => {
             <MobileIcon onClick={handleClick}>
               {click ? <FaTimes /> : <FaBars />}
             </MobileIcon>
-            <NavMenu onClick={handleClick}>
+            <NavMenu onClick={handleClick} click={click}>
               <NavItem>
                 <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="services">Services</NavLinks>
+              </NavItem>
+              <NavItem>
+                <NavLinks to="/products">Products</NavLinks>
               </NavItem>
             </NavMenu>
           </NavbarContainer>
